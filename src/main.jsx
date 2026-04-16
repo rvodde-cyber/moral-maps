567ba0e0f59b11b725636455687da168bc8261ae
rvodde-cyber <r.vodde@gmail.com>
2026-04-16 15:57:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'    /* Let op de ./ voor de naam */
+import './index.css'           /* Zorg dat dit naar je Teal CSS verwijst */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
